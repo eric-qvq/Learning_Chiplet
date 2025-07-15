a1df6b2fe437c4c9b1fc636006864dc0973d8f84
ZhulinZheng <hziee_zzl@163.com>
2025-07-15 19:26:25 +0800

    Update the scale-sim-v2 benchmark!

--- benchmark/test/scalesim/scalesim.cpp
+++ benchmark/test/scalesim/scalesim.cpp
@@ -9,18 +9,20 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-
+#include <sstream>
+#include <vector>
+#include <stdexcept>
 #include "apis_c.h"
 #include "../../interchiplet/includes/pipe_comm.h"
 
+using namespace std;
 #define CIM_ROW  16
 #define CIM_COL  4
 #define MAX_NUM  64
 
 InterChiplet::PipeComm global_pipe_comm;
 /**
- * 核心函数，由每个线程都会运行一次本函数，
- * 根据线程编号不同计算出位于结果矩阵不同位置的数据。
+ * compugte the output of cim model by chiplet idX and idY
  */
 
 void cim_model(
@@ -58,8 +60,47 @@ void cim_model(
     }
 }
 
+double sum_csv_column(const string& filename, int column_index) {
+    ifstream file(filename);
+    if (!file.is_open()) {
+        throw runtime_error("can't open file: " + filename);
+    }
+
+    string line;
+    double sum = 0.0;
+    bool is_first_line = true;
+
+    while (getline(file, line)) {
+        // jump empty line
+        if (line.empty()) continue;
+
+        stringstream ss(line);
+        string cell;
+        vector<string> cells;
+        
+        while (getline(ss, cell, ',')) {
+            cells.push_back(cell);
+        }
+
+        // jump firs line
+        if (is_first_line) {
+            is_first_line = false;
+            continue;
+        }
+
+
+        try {
+            sum += stod(cells[column_index]);
+        } catch (const invalid_argument& e) {
+            cerr << "warnning: can't translate '" << cells[column_index] << "' to number" << endl;
+        }
+    }
+
+    return sum;
+}
+
 int main(int argc, char** argv) {
-    // 读取本进程所代表的chiplet编号
+    // get chiplet id
 
     int idX = atoi(argv[1]);
     int idY = atoi(argv[2]);
@@ -90,9 +131,10 @@ int main(int argc, char** argv) {
         output, 
         shift_num         
     );
-    system("pip3 install -r /home/zzl/ws2/master_v2/Chiplet_Heterogeneous_newVersion/scale-sim-v2/requirements.txt");
-    system("cd /home/zzl/ws2/master_v2/Chiplet_Heterogeneous_newVersion/scale-sim-v2/scalesim;python3 scale.py -c ./configs/scale.cfg -t ./topologies/CSV/FasterRCNN.csv");
+    //system("pip3 install -r /home/zzl/ws2/master_v2/Chiplet_Heterogeneous_newVersion/scale-sim-v2/requirements.txt");
+    system("cd /home/zzl/ws2/master_v2/Chiplet_Heterogeneous_newVersion/scale-sim-v2/scalesim;python3 scale.py -c ../configs/scale.cfg -t ../topologies/conv_nets/test.csv");
 
+    time_end += sum_csv_column("/home/zzl/ws2/master_v2/Chiplet_Heterogeneous_newVersion/scale-sim-v2/test_runs/scale_example_run_32x32_os/COMPUTE_REPORT.csv", 1);
     fileName = InterChiplet::sendSync(idX, idY, 1, 2);
     global_pipe_comm.write_data(fileName.c_str(), output, sizeof(int8_t) * 256*test_num);
     std::cout << "simulator power of cost " << ":" << sizeof(int8_t) * 256*test_num*8*0.024 << "pJ" << std::endl;
